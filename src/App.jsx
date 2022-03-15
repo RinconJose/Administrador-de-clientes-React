ab22a013edecb447d768315aac3b6bb4df549ffb
David <david@150porciento.com>
2022-03-15 06:25:57 -0500

    Starting project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <h1>Desde app</h1>
+  )
+}
+
+export default App
