3b311bd8d9cb2fc474a68300cc48c21e14d0d34c
Jose Rincon <jose@150porciento.com>
2022-03-18 06:41:55 -0500

    Adding Link component in react router 6

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, Link } from 'react-router-dom'
 
 const Layout = () => {
   return (
@@ -6,14 +6,14 @@ const Layout = () => {
         <div className='md:w-1/4 bg-blue-900 px-5 py-10'>
           <h2 className='text-4xl font-black text-center text-white'>CRM - Clientes</h2>
           <nav className='mt-10'>
-            <a 
+            <Link 
               className='text-white text-2xl block mt-2 hover:text-blue-300'
-              href="/clientes"
-            >Clientes</a>
-            <a 
+              to="/clientes"
+            >Clientes</Link>
+            <Link 
               className='text-white text-2xl block mt-2 hover:text-blue-300'
-              href="/clientes/nuevo"
-            >Nuevo cliente</a>
+              to="/clientes/nuevo"
+            >Nuevo cliente</Link>
           </nav>
         </div>
         <div className='md:w-3/4'>
